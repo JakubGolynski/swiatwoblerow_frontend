ef3bd7782e71a01654a4f4c3641a530be538d7b7
jakub <jakub.golynski@o2.pl>
2024-10-10 10:43:20 +0200

    update v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,28 +11,22 @@ import { UserComponent } from './components/user/user.component';
 import { ProductComponent } from './components/product/product.component';
 import { AuthInterceptorService } from './components/services/auth/interceptor/auth-interceptor.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AuthComponent,
-    CategoryComponent,
-    ProductComponent,
-    UserComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        AuthComponent,
+        CategoryComponent,
+        ProductComponent,
+        UserComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        AppRoutingModule], providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptorService,
+            multi: true
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
